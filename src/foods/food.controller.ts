a632ff5bdf95dd5fd24553866584de3fa6212521
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-21 07:26:18 -0500

    add: Food controller

---
+++ src/foods/food.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Delete, HttpCode } from '@nestjs/common';
+import { FoodService } from './food.service';
+import { CreateFoodDto } from './dto/create-food.dto';
+import { Food } from './food.entity';
+
+@Controller('food')
+export class FoodController {
+  //Instancia del servicio FoodService
+  constructor(private readonly serviceFood: FoodService) {}
+
+  @Post('save')
+  @HttpCode(201)
+  create(@Body() CreateFoodDto: CreateFoodDto): Promise<Food> {
+    return this.serviceFood.create(CreateFoodDto);
+  }
+
+  @Get()
+  findAll(): Promise<Food[]> {
+    return this.serviceFood.findAll();
+  }
+
+  @Get(' id ') 
+  findOne(@Param(' id ', ParseIntPipe) id: number): Promise<Food> {
+    return this.serviceFood.findOne(id);
+  }
+
+  @Delete(' delete/id ')
+  delete(@Param(' id ') id: string): Promise<void> {
+    return this.serviceFood.delete(id);
+  }
+
+
+}
