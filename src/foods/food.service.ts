8762d9cf7258fd49aa2330cf4968e9bfcee8ba5c
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-21 07:27:23 -0500

    add: metodos CRUD

--- src/foods/food.service.ts
+++ src/foods/food.service.ts
@@ -6,17 +6,31 @@ import { CreateFoodDto } from './dto/create-food.dto';
 
 @Injectable()
 export class FoodService {
-    constructor(@InjectRepository(Food) private readonly foodRespository: Repository  <Food> ){}
+  constructor(
+    @InjectRepository(Food) private readonly foodRespository: Repository<Food>,
+  ) {}
 
-    create(CreateFoodDto: CreateFoodDto): Promise <Food>{
-        const food = new Food();
-        food.name = CreateFoodDto.name;
-        food.description = CreateFoodDto.description;
-        food.category = CreateFoodDto.category;
-        food.image = CreateFoodDto.image;
-        food.price = CreateFoodDto.price;
+  create(CreateFoodDto: CreateFoodDto): Promise<Food> {
+    //Crea un objeto FOOD
+    const food = new Food();
+    food.name = CreateFoodDto.name;
+    food.description = CreateFoodDto.description;
+    food.category = CreateFoodDto.category;
+    food.image = CreateFoodDto.image;
+    food.price = CreateFoodDto.price;
+    //Metodo que crea una tabla TypeORM
+    return this.foodRespository.save(food);
+  }
 
-        return this.foodRespository.save(food);
+  async findAll(): Promise<Food[]> {
+    return this.foodRespository.find();
+  }
 
-    }
+  findOne(id: number): Promise<Food> {
+    return this.foodRespository.findOneBy({ id: id });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.foodRespository.delete(id);
+  }
 }
