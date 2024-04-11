6a1765c893bdf08b7c1d286a3ac572d8b026141f
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-04-11 07:43:57 -0500

    variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FoodModule } from './foods/food.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'menu',
-     
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+
       synchronize: true,
       autoLoadEntities: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
     }),
-    FoodModule
+    FoodModule,
   ],
   controllers: [AppController],
   providers: [AppService],
